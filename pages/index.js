8d378ccd439dd4bf9bb25945d3e6ab4f439f1405
masa1203 <msys.sawada@gmail.com>
2021-12-30 18:34:22 +0900

    finish

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,46 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData, getStaticPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
-    <>
-      <h1 className="title">Welcome!</h1>
-      <Link href="/posts/first-post">
-        <a>Link to first-post</a>
-      </Link>
-    </>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>これはSawady.devのブログです</p>
+        <p>まぁ見てみてくださいな</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  console.log(allPostsData);
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
